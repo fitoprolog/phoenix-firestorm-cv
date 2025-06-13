1bdbc3038f0ca1bb655271cf1028fafe1c277e9a
fitoprolog <37358270+fitoprolog@users.noreply.github.com>
2025-06-12 21:02:02 -0700

    Fix bridge headers and add license

--- scripts/pybridge_poc.py
+++ scripts/pybridge_poc.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
+"""
+@file pybridge_poc.py
+@brief Proof of concept client for the Python bridge
+
+$LicenseInfo:firstyear=2024&license=viewerlgpl$
+Second Life Viewer Source Code
+Copyright (C) 2010, Linden Research, Inc.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation;
+version 2.1 of the License only.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+Linden Research, Inc., 945 Battery Street, San Francisco, CA  94111  USA
+$/LicenseInfo$
+"""
+
 import socket
 import json
 import base64
